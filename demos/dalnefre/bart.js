dd7c7a94dfca84cd24c4c141124b46f8268ebd0e
Dale Schumacher <dalnefre@yahoo.com>
2019-10-13 15:38:10 -0600

    code representations

--- demos/dalnefre/bart.js
+++ demos/dalnefre/bart.js
@@ -350,6 +350,162 @@ var BART = (function (self) {
     return crlf;
   };
 
+  // chardump
+  let chardump = function chardump(string, offset, count, separator, prefix, suffix, replace) {
+    offset = offset || 0;
+    count = count || string.length;
+    separator = separator || '';
+    prefix = prefix || '';
+    suffix = suffix || '';
+    replace = replace || function (code) {
+        if ((code < 0x20)
+        || ((0x7F <= code) && (code < 0xA0))) {  // replace control codes
+          return 0xB7;  // center-dot
+        }
+        return code;
+    }
+    var output = '';
+    var index = 0;
+    while (index < count) {
+      if ((offset + index) < string.length) {
+        if (index > 0) {
+          output += separator;
+        }
+        output += prefix;
+        var code = string.codePointAt(offset + index);
+        output += String.fromCodePoint(replace(code));
+        output += suffix;
+      }
+      ++index;
+    }
+    return output;
+  };
+  // hexdump
+  let hexdump = function hexdump(string, offset, count, separator, prefix, suffix) {
+    offset = offset || 0;
+    count = count || string.length;
+    separator = separator || ' ';
+    prefix = prefix || '';
+    suffix = suffix || '';
+    var output = '';
+    var index = 0;
+    while (index < count) {
+      if ((offset + index) < string.length) {
+        if (index > 0) {
+          output += separator;
+        }
+        output += prefix;
+        var hex = string.codePointAt(offset + index).toString(16);
+        if (hex.length > 1) {
+          output += hex;
+        } else {
+          output += '0' + hex;
+        }
+        output += suffix;
+      }
+      ++index;
+    }
+    return output;
+  };
+  // Binary Octet-Stream Encoding
+  let valueToBOSE = function valueToBOSE(value) {
+    let integerToBOSE = function integerToBOSE(n) {
+      n = 0 + n;  // force number representation
+      var output = "";
+      if ((n >= -64) && (n <= 126)) {  // small integer range
+        output += String.fromCodePoint(0x80 + n);
+      } else {
+        if (n < 0) {
+          output += String.fromCodePoint(0x18);  // negative integer, 0 padding
+        } else {
+          output += String.fromCodePoint(0x10);  // positive integer, 0 padding
+        }
+        if ((n >= -32768) && (n <= 32767)) {  // 16-bit range
+          output += String.fromCodePoint(0x82);  // 2-byte size
+          n >>>= 0;  // force 32-bit integer representation
+          output += String.fromCodePoint(n & 0xFF);  // LSB
+          n >>= 8;
+          output += String.fromCodePoint(n & 0xFF);  // MSB
+        } else {
+          output += String.fromCodePoint(0x84);  // 4-byte size
+          n >>>= 0;  // force 32-bit integer representation
+          output += String.fromCodePoint(n & 0xFF);  // LSB
+          n >>= 8;
+          output += String.fromCodePoint(n & 0xFF);
+          n >>= 8;
+          output += String.fromCodePoint(n & 0xFF);
+          n >>= 8;
+          output += String.fromCodePoint(n & 0xFF);  // MSB
+        }
+      }
+      return output;
+    };
+    let stringToBOSE = function stringToBOSE(s) {
+      s = "" + s;  // force string representation
+      var output = "";
+      if (s.length == 0) {
+        output += String.fromCodePoint(0x0F);  // empty string
+      } else {
+        output += String.fromCodePoint(0x0C);  // UTF-16 String
+        output += integerToBOSE(s.length);
+        var i = 0;
+        while (i < s.length) {
+          var c = s.charCodeAt(i);  // grab UTF-16 character
+          output += String.fromCodePoint(c >>> 8);  // MSB
+          output += String.fromCodePoint(c & 0xFF);  // LSB
+          ++i;
+        }
+      }
+      return output;
+    };
+    var output = "";
+    if (value === null) {
+      output += String.fromCodePoint(0xFF);  // null
+    } else if (value === true) {
+      output += String.fromCodePoint(0x00);  // true
+    } else if (value === false) {
+      output += String.fromCodePoint(0x01);  // false
+    } else if (typeof value === 'number') {
+      // FIXME: all numbers are treated as 32-bit integers, for now...
+      output += integerToBOSE(value);
+    } else if (typeof value === 'string') {
+      output += stringToBOSE(value);
+    } else if (Array.isArray(value)) {
+      if (value.length == 0) {
+        output += String.fromCodePoint(0x02);  // empty array
+      } else {
+        output += String.fromCodePoint(0x06);  // counted array
+        var content = integerToBOSE(value.length);  // count field
+        value.forEach(
+          function (item, index, array) {
+            content += valueToBOSE(item);
+          }
+        );
+        output += integerToBOSE(content.length);  // size field
+        output += content;
+      }
+    } else if (typeof value === 'object') {
+      let keys = Object.getOwnPropertyNames(value);
+      if (keys.length == 0) {
+        output += String.fromCodePoint(0x03);  // empty object
+      } else {
+        output += String.fromCodePoint(0x07);  // counted object
+        var content = integerToBOSE(keys.length);  // count field
+        keys.forEach(
+          function (key, index, array) {
+            content += stringToBOSE(key);
+            content += valueToBOSE(value[key]);
+          }
+        );
+        output += integerToBOSE(content.length);  // size field
+        output += content;
+      }
+    } else {  // substitute `null` for unsupported value type
+      output += String.fromCodePoint(0xFF);
+    }
+    return output;
+  };
+
   // exports
   self.workspaceToCRLF = workspaceToCRLF;
   return self;
